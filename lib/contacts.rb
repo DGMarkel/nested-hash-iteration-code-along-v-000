5f72c991b44c67848b0c8f0e967c2408b959303a
David Markel <davidgmarkel@gmail.com>
2018-03-15 19:52:30 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, person_info_hash|
     person_info_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
-        data.delete_if{|ice_cream| ice_cream == "strawberry")
+        data.delete_if{|ice_cream| ice_cream == "strawberry"}
       end
     end
   end
